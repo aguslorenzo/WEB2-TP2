718872709878cdd63a15adf7554c4c1104af6d30
Agustina <aguslorenzo21@gmail.com>
2022-11-07 20:52:01 -0300

    inicio parcial de js

---
+++ js/app.js
@@ -0,0 +1,30 @@
+"use strict"
+const URL = "http://localhost/WEB2-TPE2/api/parks";
+
+async function getAll(){
+
+    try {
+        let response = await fetch(URL);
+        if (!response.ok) {
+            throw new Error ('No existe');
+        }
+        let parks = await response.json();
+    
+        showParks(parks);
+    } catch(e){
+        console.log(e);
+    }
+
+}
+
+function showParks(parks){
+    let ul = document.querySelector("#park-list");
+    ul.innerHTML = "";
+    for (const park of parks) {
+        // aca mi problema es q me muestra el id de la provincia
+        ul.innerHTML += `<li> ${park.name} | ${park.description} | ${park.price} | ${park.id_province_fk} </li>` 
+        console.log(park);
+    }
+}
+
+getAll();
